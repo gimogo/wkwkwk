63e53e120eda6625d89328b179175af773662d09
John <37008188+gimogo@users.noreply.github.com>
2021-05-04 04:19:26 +0700

    Update settings.py

--- art_of_tea/settings.py
+++ art_of_tea/settings.py
@@ -176,7 +176,8 @@ USE_TZ = True
 #MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT= ''#os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+#STATIC_ROOT= ''#os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ( os.path.join('static'), )
 
